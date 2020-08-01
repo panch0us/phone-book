7c62b59c4355125789e7ebd8f0dc99d910b4dfee
panch0us <godbryansk@yandex.ru>
2020-08-01 17:57:25 +0300

    Добавление собранного exe-файла для запуска телефонного справочника

--- tab_calculations.py
+++ tab_calculations.py
@@ -8,7 +8,7 @@ class Appg(tkinter.Frame):
         super().__init__(parent)
 
         self.parent = parent
-
+        # Создаем 3 фрейма (левый, центральный и правый)
         self.frame_left = tkinter.LabelFrame(text='АППГ')
         self.frame_left.place(in_=self, x=5, y=5, height=600, width=390)
 
@@ -21,102 +21,46 @@ class Appg(tkinter.Frame):
         self.frame_bot = tkinter.LabelFrame()
         self.frame_bot.place(in_=self, x=5, y=605, height=33, width=1175)
 
-        self.init_ui()
+        # Создаем 3 функции для различных вычислений
+        self.appg()
+        self.percent()
+        self.percent_appg()
 
-    def init_ui(self):
-        """ Создаем виджет расчета АППГ для текущего окна"""
-        ##### ЭЛЕМЕНТЫ ЛЕВОГО ОКНА - РАССЧЕТ АППГ #####
-        # лейбл для отображения поля "текущий (анг. current) год"
-        self.label_current_year_appg = tkinter.Label(self.frame_left, text='Введите значение текущего года: ')
-        # entry - (анг. вход) - поле для ввода данных пользователем
-        self.entry_current_year_appg = tkinter.Entry(self.frame_left, width=15)
-        # устанавливаем расположение (place, анг. место) для элементов
-        self.label_current_year_appg.place(x=8, y=10)
-        self.entry_current_year_appg.place(x=220, y=10)
+    def appg(self):
+        """Создаем элементы для расчета АППГ"""
+        ##### ЭЛЕМЕНТЫ ТЕКУЩЕГО ГОДА #####
+        label_current_year = tkinter.Label(self.frame_left, text='Введите значение текущего года: ')
+        label_current_year.place(relx=0.1, rely=0.1)
+        entry_current_year = tkinter.Entry(self.frame_left, width=10)
+        entry_current_year.place(relx=0.7, rely=0.1)
 
         ##### ЭЛЕМЕНТЫ ПРОШЛОГО ГОДА #####
-        self.label_last_year_appg = tkinter.Label(self.frame_left, text='Введите значение прошлого года: ')
-        self.entry_last_year_appg = tkinter.Entry(self.frame_left, width=15)
-        # устанавливаем расположение (place, анг. место) для элементов
-        self.label_last_year_appg.place(x=8, y=45)
-        self.entry_last_year_appg.place(x=220, y=45)
+        label_last_year = tkinter.Label(self.frame_left, text='Введите значение прошлого года: ')
+        label_last_year.place(relx=0.1, rely=0.2)
+        entry_last_year = tkinter.Entry(self.frame_left, width=10)
+        entry_last_year.place(relx=0.7, rely=0.2)
 
         ##### ЭЛЕМЕНТЫ РАСЧЕТА #####
-        self.label_calculation_appg = tkinter.Label(self.frame_left, text='Результат: ')
-        # StringVar - связан с элементом lable и в его поле textvariable вносит получившее значение
-        # из переменной calculation.
+        label_calculation = tkinter.Label(self.frame_left, text='Результат: ')
+        label_calculation.place(relx=0.1, rely=0.3)
         self.calculation_appg = tkinter.StringVar()
-        self.label_calculation_result_appg = tkinter.Label(self.frame_left, textvariable=self.calculation_appg)
-        # упаковываем созданные элементы (side - сторона).
-        self.label_calculation_appg.place(x=8, y=138)
-        self.label_calculation_result_appg.place(x=75, y=138)
-
-        # линия после меню поиска в левом окне
-        sep_search_appg = ttk.Separator(self.frame_left, orient="horizontal")
-        sep_search_appg.place(x=0, y=180, relwidth=1.0)
+        label_calculation_result = tkinter.Label(self.frame_left, textvariable=self.calculation_appg)
+        label_calculation_result.place(relx=0.7, rely=0.3)
 
         ##### КНОПКИ #####
-        self.button_calculation_appg = tkinter.Button(self.frame_left, text='Рассчитать', command=self.calculation_appg)
-        # упаковываем созданные элементы (side - сторона).
-        self.button_calculation_appg.place(x=130, y=85)
+        button_calculation = tkinter.Button(self.frame_left, text='Рассчитать', command=self.calculation_def)
+        button_calculation.place(relx=0.1, rely=0.4)
 
-        ##### ЭЛЕМЕНТЫ СРЕДНЕГО ОКНА - РАССЧЕТ ПРОЦЕНТОВ #####
-        # лейбл для отображения поля "текущего (анг. current) год"
-        self.label_current_year_percent = tkinter.Label(self.frame_center, text='Введите значение текущего года: ')
-        # entry - (анг. вход) - поле для ввода данных пользователем
-        self.entry_current_year_percent = tkinter.Entry(self.frame_center, width=10)
-        # устанавливаем расположение (place, анг. место) для элементов
-        self.label_current_year_percent.place(relx=0.1, rely=0.1)
-        self.entry_current_year_percent.place(relx=0.7, rely=0.1)
-
-        ##### ЭЛЕМЕНТЫ ПРОШЛОГО ГОДА #####
-        self.label_last_year_percent = tkinter.Label(self.frame_center, text='Введите процент прошлого года: ')
-        self.entry_last_year_percent = tkinter.Entry(self.frame_center, width=10)
-        # устанавливаем расположение (place, анг. место) для элементов
-        self.label_last_year_percent.place(relx=0.1, rely=0.2)
-        self.entry_last_year_percent.place(relx=0.7, rely=0.2)
-
-        ##### ЭЛЕМЕНТЫ РАСЧЕТА #####
-        self.label_calculation_percent = tkinter.Label(self.frame_center, text='Результат: ')
-        # StringVar - связан с элементом lable и в его поле textvariable вносит получившее значение
-        # из переменной calculation.
-        self.calculation_percent = tkinter.StringVar()
-        self.label_calculation_result_percent = tkinter.Label(self.frame_center, textvariable=self.calculation_percent)
-        # упаковываем созданные элементы (side - сторона).
-        self.label_calculation_percent.place(relx=0.1, rely=0.3)
-        self.label_calculation_result_percent.place(relx=0.7, rely=0.3)
-
-        ##### КНОПКИ #####
-        self.button_calculation_percent = tkinter.Button(self.frame_center, text='Рассчитать', command=self.calculation_percent)
-
-        # упаковываем созданные элементы (side - сторона).
-        self.button_calculation_percent.place(relx=0.1, rely=0.4)
+        return entry_current_year.get(), entry_last_year.get()
 
 
-    def calculation_appg(self):
-        # получаем данные из полей enty, куда пользоватеь ввоил свои данные, при помощи метода get
-        self.current_year_appg = int(self.entry_current_year_appg.get())
-        self.last_year_appg = int(self.entry_last_year_appg.get())
-        # вычисляем АППГ
-        self.calculation_result_appg = str(round(((self.current_year_appg - self.last_year_appg) / self.last_year_appg) * 100, 1)) + "%"
-        # обновляем элемент label_calculation_result в рамке рачета, сохранив значение calculation_result в
-        # объект StingVar, на который ссылается переменная calculation
-        self.calculation_appg.set(self.calculation_result_appg)
+    def percent(self):
+        pass
+
+    def percent_appg(self):
+        pass
 
 
 
-    def calculation_percent(self):
-        # получаем данные из полей enty, куда пользоватеь ввоил свои данные, при помощи метода get
-        self.current_year_percent = int(self.entry_current_year_percent.get())
-        self.last_year_percent = float(self.entry_last_year_percent.get())
-        # вычисляем количество преступлений от процента, связанного с прошлым годом
-
-        self.result_percent = 100 + self.last_year_percent
-        self.result_sum_percent = self.current_year_percent * 100 / self.result_percent
-        # разница
-        self.difference_percent = self.current_year_percent - self.result_sum_percent
-        self.appg_percent = round(self.current_year_percent - self.difference_percent)
-
-        # обновляем элемент label_calculation_result в рамке рачета, сохранив значение calculation_result в
-        # объект StingVar, на который ссылается переменная calculation
-        self.calculation_percent.set(self.appg_percent)
+    def calculation_def(self):
+        pass
